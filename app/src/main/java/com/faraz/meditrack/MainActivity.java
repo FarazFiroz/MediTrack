5092e68a709eb922872379c49ba1199558ae2d6a
FarazFiroz <farazfiroz2472@gmail.com>
2025-10-24 12:56:42 +0530

    Camera image can be added in db, default title when adding report

--- app/src/main/java/com/faraz/meditrack/MainActivity.java
+++ app/src/main/java/com/faraz/meditrack/MainActivity.java
@@ -6,12 +6,17 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     private final int REQUEST_IMAGE = 100;
     private Uri cameraImageUri;
     private SharedPreferences tinyDb;
+    private SharedPreferences.Editor editor;
+    private ActivityResultLauncher<Intent> imageChooserLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+
+        tinyDb = getSharedPreferences("reports", MODE_PRIVATE);
+        editor = tinyDb.edit();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -56,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.fragmentContainer, new HomeFragment())
                 .commit();
 
+        imageChooserLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if(result.getResultCode() == RESULT_OK){
+                Intent data = result.getData();
+                Uri imageUri;
+
+                if(data != null && data.getData() != null){
+                    imageUri = data.getData();
+                } else {
+                    imageUri = cameraImageUri;
+                }
+
+                if(imageUri != null) showAddReportDialog(imageUri);
+            }
+        });
+
 
         binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
             @Override
@@ -65,20 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 if(id == R.id.nav_home){
                     selectedFragment = new HomeFragment();
                 } else if(id == R.id.nav_add) {
-                    Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    try {
-                        cameraImageUri = createImageFileUri();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
-
-                    Intent selectPhoto = new Intent(Intent.ACTION_PICK);
-                    selectPhoto.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                    Intent chooser = Intent.createChooser(selectPhoto, "Capture or Select an Image");
-                    chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {takePhoto});
-                    startActivityForResult(chooser, REQUEST_IMAGE);
+                    openImageChooser();
                 }
 
                 if(selectedFragment!=null){
@@ -90,35 +102,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK && data != null){
-            Uri imageUri;
-            if(data != null && data.getData() != null){
-                imageUri = data.getData();
-            } else {
-                imageUri = cameraImageUri;
-            }
-
-            View view = getLayoutInflater().inflate(R.layout.add_report_dialog, null);
-            EditText reportTitleBox = view.findViewById(R.id.addReportTitleBox);
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("Add Report")
-                    .setMessage("Enter the title for this report")
-                    .setCancelable(false)
-                    .setView(view)
-                    .setPositiveButton("Add", (dialog, which) -> {
-                        String reportTitle = reportTitleBox.getText().toString();
-                        if(reportTitle.isEmpty()) reportTitle = "com.faraz.meditrack.Report " + tinyDb.getString("reportNo", "1");
-
-                        storeData(imageUri, reportTitle);
-                    })
-                    .setNegativeButton("Cancel", (dialog, which) -> {})
-                    .show();
-        }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private Uri createImageFileUri() throws IOException {
@@ -137,10 +120,60 @@ public class MainActivity extends AppCompatActivity {
         Report report = new Report(title, image.toString(), date);
         new Thread(() -> {
             dao.insert(report);
-            List<Report> reports = dao.getAllReports();
-            for (Report r : reports) {
-                Log.d("ReportsDB", r.title + " | " + r.imageUri + " | " + r.date);
-            }
         }).start();
+        int reportNo = Integer.valueOf(tinyDb.getString("reportNo", "1")) + 1;
+        editor.putString("reportNo", String.valueOf(reportNo));
+        Toast.makeText(this, "Report added", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openImageChooser() {
+        Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            cameraImageUri = createImageFileUri();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+
+        Intent selectPhoto = new Intent(Intent.ACTION_PICK);
+        selectPhoto.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        Intent chooser = Intent.createChooser(selectPhoto, "Capture or Select an Image");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {takePhoto});
+        imageChooserLauncher.launch(chooser);
+    }
+
+    private void showAddReportDialog(Uri imageUri) {
+        View view = getLayoutInflater().inflate(R.layout.add_report_dialog, null);
+        EditText reportTitleBox = view.findViewById(R.id.addReportTitleBox);
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Add Report")
+                .setMessage("Enter the title for this report")
+                .setCancelable(false)
+                .setView(view)
+                .setPositiveButton("Add", (dialog, which) -> {
+                    String reportTitle = reportTitleBox.getText().toString();
+                    if(reportTitle.isEmpty()) reportTitle = "com.faraz.meditrack.Report " + tinyDb.getString("reportNo", "1");
+
+                    storeData(imageUri, reportTitle);
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> {})
+                .show();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        if(cameraImageUri != null){
+            outState.putString("cameraImageUri", cameraImageUri.toString());
+        }
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        if(savedInstanceState.containsKey("cameraImageUri")){
+            cameraImageUri = Uri.parse(savedInstanceState.getString("cameraImageUri"));
+        }
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
